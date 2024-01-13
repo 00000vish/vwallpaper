5a07fbf18f6d7debacfac44e27ea157d8289e1de
vish <11762008+00000vish@users.noreply.github.com>
2024-01-12 23:53:03 -0600

    parse toml

--- src/config.rs
+++ src/config.rs
@@ -1,17 +1,39 @@
-pub mod config {
+use std::u64;
+
+pub mod reader {
+    use super::Config;
+    use super::Display;
     use dirs;
-    use std::path::Path;
+    use std::{fs, path::Path};
     use toml::Table;
 
-    pub fn read_config() -> Result<Table, String> {
+    pub fn read_config() -> Result<Config, String> {
         let found = Path::new(&get_config_file_path()).exists();
         if !found {
             return Result::Err(format!(
-                "Could not find config file.\n Looking at {}.\n Exiting.",
+                "Could not find config file.\nLooking at {}.\nExiting.",
                 get_config_file_path(),
             ));
         }
+
+        let toml_str = fs::read_to_string(get_config_file_path());
+        match toml_str {
+            Err(error) => {
+                return Result::Err(format!("Could not read config file.\n{}\nExiting.", error))
+            }
+            _ => (),
+        }
+
+        let toml_map = toml_str.unwrap().parse::<Table>().unwrap();
+
+        for (key, value) in toml_map {
+            println!("{} => {} ", key, value);
+        }
         Err("error".to_string())
+    }
+
+    fn parse_display_struct() -> Result<Display, String> {
+        Err("todo".to_string())
     }
 
     fn get_config_file_path() -> String {
@@ -20,4 +42,16 @@ pub mod config {
         let file_path = format!("{}/.config/vwallpaper/config.toml", home_dir_str);
         file_path
     }
+}
+
+pub struct Config {
+    app: String,
+    displays: Vec<Display>,
+}
+
+pub struct Display {
+    name: String,
+    file: Option<String>,
+    directoy: Option<String>,
+    seconds: u64,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
-use toml::Table;
+use std::process::{self};
 
 mod config;
 
 fn main() {
-    let something = config::config::read_config();
+    let something = config::reader::read_config();
     match something {
-        Err(error) => println!("{}", error),
+        Err(error) => exit_with_msg(Some(&error)),
         Ok(_) => (),
     }
 }
 
-fn exit(message: String) {}
+fn exit_with_msg(message: Option<&str>) {
+    match message {
+        Some(n) => println!("{}", n),
+        None => (),
+    }
+    process::exit(1);
+}
